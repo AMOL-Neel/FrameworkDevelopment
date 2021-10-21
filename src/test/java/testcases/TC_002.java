d749836c63e3d3c840d564bb3ac8f60d7cb4d50c
AMOL-Neel <amolneel02@gmail.com>
2021-10-21 19:41:56 +0530

    modified : BasePage and TC_002

--- src/test/java/testcases/TC_002.java
+++ src/test/java/testcases/TC_002.java
@@ -14,41 +14,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pages.BasePage;
 
-public class TC_002 extends BasePage{
-	
-	@Before
-    public void set_up(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(implicit_wait_timeout_in_sec, TimeUnit.SECONDS);
-    }
+public class TC_002 extends BasePage {
 
-    @Test
-    public void t_02_search_for_product() {
-        driver.get(base_url);
-        String expected = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
-        String actual =driver.getTitle();
-        Assert.assertEquals("Page Title validation",expected,actual);
+	@Test
+	public void t_02_search_for_product() {
+		driver.get(base_url);
+		String expected = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		String actual = driver.getTitle();
+		Assert.assertEquals("Page Title validation", expected, actual);
 
-        //Wait and Search for product
-        WebDriverWait webDriverWait = new WebDriverWait(driver,20);
-        WebElement elementSearchBox = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("twotabsearchtextbox")));
+		// Wait and Search for product
+		WebDriverWait webDriverWait = new WebDriverWait(driver, 20);
+		WebElement elementSearchBox = webDriverWait
+				.until(ExpectedConditions.elementToBeClickable(By.id("twotabsearchtextbox")));
 
-        elementSearchBox.sendKeys("Laptop");
-        driver.findElement(By.xpath("//input[@value='Go']")).click();
+		elementSearchBox.sendKeys("Laptop");
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
 
-        //Wait for titile
-        WebDriverWait webDriverWait1 = new WebDriverWait(driver,20);
-        webDriverWait1.until(ExpectedConditions.titleIs("Amazon.in : Laptop"));
+		// Wait for titile
+		WebDriverWait webDriverWait1 = new WebDriverWait(driver, 20);
+		webDriverWait1.until(ExpectedConditions.titleIs("Amazon.in : Laptop"));
 
-        //Assertion for Page Title
-        Assert.assertEquals("Page Title validation","Amazon.in : Laptop", driver.getTitle());
-    }
-
-    @After
-    public void clean_up(){
-        driver.quit();
-    }
-
+		// Assertion for Page Title
+		Assert.assertEquals("Page Title validation", "Amazon.in : Laptop", driver.getTitle());
+	}
 
 }

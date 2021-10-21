78464354bb321c0da2bb4b7ae2aa0919373fbbcf
AMOL-Neel <amolneel02@gmail.com>
2021-10-21 19:03:52 +0530

    added new TC_002 and BasePage

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+	protected WebDriver driver;//this should never be static, if made static parallel exec of classes not possible
+	protected final static String base_url = "https://amazon.in";
+	protected final static int implicit_wait_timeout_in_sec = 20;
+
+}
